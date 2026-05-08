db0cb511e9db1d50b115359139d3baf77cc10c1c
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-08 12:30:09 +0530

    refactor of generating blocks function to now include user state

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,25 +1,33 @@
+import type { User } from '../types.js';
+
 const cell = <HTMLSpanElement>document.querySelector('.item')!;
 
 const cellSize = cell?.getBoundingClientRect().width;
 
-function generateBlock(size = 2) {
-	const block = document.createElement('div');
+function generateBlock(size = 2, user: User, player: boolean) {
+	const block = document.createElement('button');
 
 	const isVertical = Math.random() < 0.5 ? true : false;
 
 	block.style.height = `${(cellSize && (isVertical ? cellSize * size : cellSize)) || 0}px`;
 	block.style.width = `${(cellSize && (isVertical ? cellSize : cellSize * size)) || 0}px`;
-	block.classList.add('block');
+	block.classList.add('block', user);
+
+	if (!player) block.classList.add('hidden');
 
 	return block;
 }
 
-function generateBlocks() {
-	const block1 = generateBlock(3);
-	const block2 = generateBlock(2);
-	const block3 = generateBlock(2);
+function generateBlocks(user: User, player = true) {
+	const block1 = generateBlock(3, user, player);
+	const block2 = generateBlock(2, user, player);
+	const block3 = generateBlock(2, user, player);
 
-	return [block1, block2, block3];
+	return [block1, block2, block3] as [
+		HTMLButtonElement,
+		HTMLButtonElement,
+		HTMLButtonElement,
+	];
 }
 
 export { generateBlocks };
