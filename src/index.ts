2125dc394d97b5300c2db5820a8da65616476e67
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-08 12:41:00 +0530

    generate blocks for both players

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-const gameArea = <HTMLDivElement>document.querySelector('.game-area')!;
-const player1Grid = <HTMLDivElement>document.querySelector('.player1-grid')!;
-const player2Grid = <HTMLDivElement>document.querySelector('.player2-grid')!;
-const player1GridPositions = <HTMLSpanElement[]>[
-	...document.querySelectorAll('.player1-grid .item'),
-];
-const player2GridPositions = <HTMLSpanElement[]>[
-	...document.querySelectorAll('.player2-grid .item'),
-];
+import { elements } from './elements.js';
+import type { User } from './types.js';
+import { generateBlocks } from './utils/helpers.js';
 
-console.log(gameArea);
-console.log(player1Grid);
-console.log(player1GridPositions);
-console.log(player2Grid);
-console.log(player2GridPositions);
+const user1: User = 'player1';
+const user2: User = 'player2';
+const player2 = false;
+
+(elements['player1BlocksArea'] as HTMLElement).append(...generateBlocks(user1));
+(elements['player2BlocksArea'] as HTMLElement).append(...generateBlocks(user2, player2));
